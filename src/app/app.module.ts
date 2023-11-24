a111a40c6b538315423f3e05a56337b8b49eabeb
chalscc <carlescatalancalabuig@gmail.com>
2023-11-24 15:07:26 +0100

    Cración de modulos countries y shared

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    AppComponent,
+    SharedModule
+  ],
+  exports: [],
+  providers: [],
+})
+export class AppModule { }
